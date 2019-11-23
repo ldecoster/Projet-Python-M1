76c11071ffd0dd6bd518d54574f13ef746335569
Somonntc <36538310+Somonntc@users.noreply.github.com>
2019-11-23 17:25:52 +0100

    Add files via upload
    Je t'enverrai un message pour expliquer à peu près mon idée de fonctionnement, ce qu'il manque, ce qui est déjà fait.
    Le code est un peu commenté, pour l'instant je fais mes test directement dans les classes ou j'ai besoin (je test les fonctions de player dans player pour être plus clair)

---
+++ enemy.py
@@ -0,0 +1,41 @@
+import random
+
+from Projet.player import player
+
+
+class enemy:
+    def __init__(self):
+        self.value = []
+        self.stats = self.readstats()
+        self.attack(10)
+
+    def readstats(self):
+        f = open("enemy.txt", 'r')
+        lignes = f.readlines()
+        f.close()
+        for ligne in lignes:
+            self.value.append(int(ligne))
+        #print(self.value)
+        return self.value
+
+    def attack(self, damage):
+        # rnd to see if we crit
+        print("before hit", player.stats[0])
+        rnd_crit = random.randint(1, 100)
+        if rnd_crit <= int(self.stats[5]):
+            print("Critical hit")
+            damage = damage * 2
+        player.defense(damage)
+        print("After hit", player.stats[0])
+
+
+    def lose_life(self, damage):
+        life = self.stats[0]
+        life_int = int(life)
+        life_int -= damage
+        self.stats[0] = life_int
+        return self.stats[0]
+
+    #Pareil, faire une fonction heal etc, Fonction commune ?
+
+enemy = enemy()
---
+++ map.py
@@ -0,0 +1,39 @@
+import numpy as np
+from random import randint
+SIZE = 7
+
+class tab_map:
+    def __init__(self):
+        self.tab_map = []
+        room_start, room_end = self.initmap()
+        print(self.tab_map)
+        print("Start room : ", room_start, ", ", "End room", room_end)
+        #  print("Map from __init__ : ", self.tab_map)
+
+    def initmap(self):
+        n = SIZE
+        x = np.zeros(n)
+        y = np.zeros(n)
+        for i in range(1, n):
+            val = randint(1, 4)
+            if val == 1:
+                x[i] = x[i - 1] + 1
+                y[i] = y[i - 1]
+            elif val == 2:
+                x[i] = x[i - 1] - 1
+                y[i] = y[i - 1]
+            elif val == 3:
+                x[i] = x[i - 1]
+                y[i] = y[i - 1] + 1
+            else:
+                x[i] = x[i - 1]
+                y[i] = y[i - 1] - 1
+
+        for i in range(SIZE):
+            self.tab_map.append(((x[i], y[i]), 1))
+
+        room_start = self.tab_map[0]
+        room_end = self.tab_map[SIZE-1]
+        return room_start, room_end
+
+tab_map = tab_map()
